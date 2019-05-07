178f64c143df470ac7c60258343d573fcf25ecd1
Daniel Scott <dasc0325@colorado.edu>
2019-05-07 15:33:07 -0600

    v1.3 [published]: updated the bios
     - added portrait contexts to each member of the team
     - added short blurbs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,37 +15,37 @@ export class HomeComponent implements OnInit {
   teamDescriptions = {
     timFace: [
       'Timothy Passmore, PhD', 
-      'SAPANA (Strategies for Aerospace Policy and Norm Adoption) is a student research team at the University of Colorado Boulder, brought together as part of the Department of Defense’s Designing for Defense program. Our collective expertise include aerospace engineering, mechanical engineering, political science, economics, and business. The team includes two recent PhD graduates, three Master’s students, and two undergraduate students.',
+      'Tim Passmore is a political scientist who studies American foreign policy, civil and interstate conflict, and international law. His current research addresses the role played by intergovernmental organizations in conflict reduction. He has taught university courses for the last 9 years and will begin as an Assistant Professor of International Studies at the Virginia Military Institute in Fall 2019.',
       '../../assets/tim.jpg'
     ],
     akhilFace: [
       'Akhil Rao, PhD', 
-      'New problems require new solutions, and old problems need fresh perspective. SAPANA is a student-based research consultancy firm offering expert insights into current and future problems relating to space. Our expertise spans aerospace and mechanical engineering, political science, economics, computer science, and business. We are independent, inquisitive, and dedicated to excellence.',
+      'Akhil Rao is an economist working on the economic management of low-Earth orbit. His research focuses on the use of economic policy to control collision risk and debris growth, with an eye to preventing Kessler Syndrome in valuable orbits. He will be an Assistant Professor of Economics at Middlebury College starting Fall 2019.',
       '../../assets/akhil.png'
     ],
     danielFace: [
       'Daniel Scott', 
-      'Daniel is a right proper lad',
+      'Daniel Scott is a computer science major and business minor at the University of Colorado - Boulder. He is currently employed by the Department of Computer Science at CU as a course assistant. In the summer, he will be a software engineer intern at VISA where he will work primarily on VIP Middleware Development.',
       '../../assets/daniel.jpg'
     ],
     britneeFace: [
       'Britnee Leopold', 
-      'Britnee is a right proper lad',
+      'Britnee Leopold is an undergraduate aerospace engineering student working towards a master’s degree. She is experienced as a researcher, project lead, test engineer, and field engineer. Britnee is currently employed as an intern by MD5 and is working to help solve national security problems in innovative ways.',
       '../../assets/britnee.jpg'
     ],
     mattFace: [
       'Matt Roberts', 
-      'Matt is a right proper lad',
+      'Matt Roberts is a Mechanical Engineering major at the University of Colorado - Boulder. He is an AFROTC Cadet who has earned his Pilot slot and will commission as a 2nd Lt. in May 2020. This summer he will be working for NOAA on the FIREX-AQ project which uses airborne instruments to analyze wildfire smoke composition.',
       '../../assets/matt.jpg'
   ],
     kimFace: [
       'Kimberly DeVore', 
-      'Kim is a right proper lad',
+      'Kim is a mechanical engineering master’s student. She is currently employed at the National Institute of Standards and Technology (NIST) where she performs a wide variety of functions.',
       '../../assets/kim.jpg'
     ],
     meerFace: [
       'Meer Baker', 
-      'Meer is a right proper lad',
+      'Meer Baker is an aerospace engineering student focusing on astrodynamics and controls. He is currently working in the Bioastronautics lab modeling computer simulated landings. His research interests are in Space debris and interplanetary trajectories.',
       '../../assets/meer.jpg'
   ],
 
